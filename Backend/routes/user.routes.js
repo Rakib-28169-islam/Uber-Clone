e4d11e78864db49d7c0f26ecc95d1af83ea8df06
Rakib-28169-islam <rakibulislam28169@gmail.com>
2024-12-28 22:56:06 +0600

    docs for users/register

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {body} = require("express-validator");
-const userController = require("../controllers/user.controller");
+const {registerUser} = require("../controllers/user.controller");
 
 router.post("/register",[
     body('fullName.fName').isLength({min:3}).withMessage("First Name should be at least 3 characters long"),
     body('email').isEmail().withMessage("Please enter a valid email"),
     body('password').isLength({min:4}).withMessage("Password should be at least 4 characters long")
-],
-userController.registerUser
+],registerUser
 )
 
 module.exports = router;
