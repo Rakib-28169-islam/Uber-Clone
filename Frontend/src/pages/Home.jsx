938f20bd44f288d145c8ea2fa91ea73042d8d138
Rakib-28169-islam <rakibulislam28169@gmail.com>
2025-01-12 23:50:55 +0600

    gsap animation testing in home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,6 +1,38 @@
-import React from "react";
-
+import React, { useEffect, useRef, useState } from "react";
+import  {useGSAP} from '@gsap/react'
+import { gsap } from 'gsap';
 const Home = () => {
+
+  //ref
+  const suggestionsPanel = useRef(null);
+
+  const [pickup,setPickup] = useState('');
+  const [drop,setDrop] = useState('');
+  const [openPanel,setOpenPanel] = useState(false);
+
+   const submitHandler = e =>{
+    e.preventDefault();
+
+    console.log(pickup,drop);
+
+   }
+
+   useEffect(()=>{
+    if(openPanel)
+    {
+      gsap.to(suggestionsPanel.current,{
+        height:'70%'
+      })
+    }
+    else
+    {
+      gsap.to(suggestionsPanel.current,{
+        height:'0%'
+      })
+    }
+   },[openPanel])
+
+
   return (
     <div className="h-screen relative overflow-hidden">
       <img
@@ -23,22 +55,33 @@ const Home = () => {
                         <i className="ri-arrow-down-wide-line"></i>
                     </h5> */}
                     <h4 className='text-2xl font-semibold'>Find a trip</h4>
-                    <form >
+
+                    <form onSubmit={(e)=>submitHandler(e)}>
                         <div className="line absolute h-16 w-1 top-[35%] left-10 bg-gray-700 rounded-full"></div>
                         <input
                            
+                           onClick ={()=>setOpenPanel(true)}
+                           onChange={(e)=>setPickup(e.target.value)}
+                           value={pickup}
+
+
                             className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5'
                             type="text"
                             placeholder='Add a pick-up location'
                         />
                         <input
                            
+                           onClick ={()=>setOpenPanel(true)}
+                           onChange={(e)=>setDrop(e.target.value)}
+                           value={drop}
+
+
                             className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3'
                             type="text"
                             placeholder='Enter your destination' />
                     </form>
                 </div>
-                <div  className='h-0 bg-red-500 '>
+                <div  ref={suggestionsPanel} className='h-0 bg-red-500 '>
                     
                 </div>
             </div>
